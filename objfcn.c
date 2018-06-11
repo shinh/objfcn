0517f617d6a68215c9987ed3d812b870059bf1e7
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-06-11 17:33:36 +0900

    Split objopen to a few functions

--- objfcn.c
+++ objfcn.c
@@ -110,7 +110,7 @@ static void init(void) {
 
 #endif
 
-char* read_file(const char* filename) {
+static char* read_file(const char* filename) {
   FILE* fp = NULL;
   char* bin = NULL;
   long filesize = 0;
@@ -151,44 +151,124 @@ error:
   return NULL;
 }
 
-void* objopen(const char* filename, int flags) {
-  char* bin = NULL;
-  obj_handle* obj = NULL;
-  Elf_Ehdr* ehdr = NULL;
+static size_t relocate(obj_handle* obj,
+                       const char* bin,
+                       Elf_Sym* symtab,
+                       const char* strtab,
+                       char** addrs,
+                       int code_size_only) {
+  size_t code_size = 0;
+  Elf_Ehdr* ehdr = (Elf_Ehdr*)bin;
+  Elf_Shdr* shdrs = (Elf_Shdr*)(bin + ehdr->e_shoff);
+  for (int i = 0; i < ehdr->e_shnum; i++) {
+    Elf_Shdr* shdr = &shdrs[i];
+    int has_addend = shdr->sh_type == SHT_RELA;
+    size_t relsize = has_addend ? sizeof(Elf_Rela) : sizeof(Elf_Rel);
+    int relnum = shdr->sh_size / relsize;
+    char* target_base = addrs[shdr->sh_info];
 
-#if !OBJFCN_SPLIT_ALLOC
-  if (!code) {
-    init();
-  }
-  if (code == MAP_FAILED) {
-    return NULL;
-  }
+    if ((shdr->sh_type != SHT_REL && shdr->sh_type != SHT_RELA) ||
+        !(shdrs[shdr->sh_info].sh_flags & SHF_ALLOC)) {
+      continue;
+    }
+
+    for (int j = 0; j < relnum; j++) {
+      Elf_Rela* rel = (Elf_Rela*)(bin + shdr->sh_offset + relsize * j);
+      char* target = target_base + rel->r_offset;
+      Elf_Sym* sym = &symtab[ELF_R_SYM(rel->r_info)];
+      int addend = has_addend ? rel->r_addend : 0;
+      char* sym_addr = NULL;
+
+      if (!code_size_only) {
+        switch (ELF_ST_TYPE(sym->st_info)) {
+          case STT_SECTION:
+            sym_addr = addrs[sym->st_shndx];
+            break;
+
+          case STT_FUNC:
+          case STT_OBJECT:
+            sym_addr = (char*)sym->st_value;
+            break;
+
+          case STT_NOTYPE:
+            if (sym->st_shndx == SHN_UNDEF) {
+              sym_addr = (char*)dlsym(RTLD_DEFAULT, strtab + sym->st_name);
+              if (sym_addr == NULL) {
+                sprintf(obj_error, "failed to resolve %s",
+                        strtab + sym->st_name);
+                return 0;
+              }
+            } else {
+              sym_addr = addrs[sym->st_shndx];
+            }
+            break;
+
+          default:
+            sprintf(obj_error, "unsupported relocation sym %d",
+                    ELF_ST_TYPE(sym->st_info));
+            return 0;
+        }
+        //fprintf(stderr, "%d %s target=%p sym_addr=%p addend=%d\n",
+        //        j, strtab + sym->st_name, target, sym_addr, addend);
+      }
+
+      switch (ELF_R_TYPE(rel->r_info)) {
+#ifdef R_64
+        case R_64:
+          if (!code_size_only)
+            *(uint64_t*)target = (uint64_t)sym_addr + addend;
+          break;
 #endif
 
-  bin = read_file(filename);
-  if (bin == NULL) {
-    return NULL;
+#ifdef R_PC32
+        case R_PC32:
+          if (!code_size_only)
+            *(uint32_t*)target = (sym_addr - target) + addend;
+          break;
+#endif
+
+#ifdef R_PLT32
+        case R_PLT32:
+          if (code_size_only) {
+            code_size += 6 + 8;
+          } else {
+            void* dest = sym_addr;
+#if defined(__x86_64__)
+            sym_addr = alloc_code(obj, 6 + 8);
+            sym_addr[0] = 0xff;
+            sym_addr[1] = 0x25;
+            *(uint32_t*)(sym_addr + 2) = 0;
+            *(uint64_t*)(sym_addr + 6) = (uint64_t)dest;
+#endif
+            *(uint32_t*)target = (sym_addr - target) + addend;
+          }
+          break;
+#endif
+
+#if defined(__x86_64__)
+        case R_X86_64_REX_GOTPCRELX:
+          if (code_size_only) {
+            code_size += 8;
+          } else {
+            void* dest = sym_addr;
+            sym_addr = alloc_code(obj, 8);
+            *(uint64_t*)(sym_addr) = (uint64_t)dest;
+            *(uint32_t*)target = (sym_addr - target) + addend;
+          }
+          break;
+#endif
+
+        default:
+          sprintf(obj_error, "Unknown reloc: %ld", ELF_R_TYPE(rel->r_info));
+          return 0;
+      }
+    }
   }
+  return code_size;
+}
 
-  obj = (obj_handle*)malloc(sizeof(obj_handle));
-  if (obj == NULL) {
-    sprintf(obj_error, "malloc failed");
-    free(bin);
-    return NULL;
-  }
-  memset(obj, 0, sizeof(*obj));
-
-  ehdr = (Elf_Ehdr*)bin;
-  if (memcmp(ehdr->e_ident, ELFMAG, 4)) {
-    sprintf(obj_error, "%s is not ELF", filename);
-    free(bin);
-    free(obj);
-    return NULL;
-  }
-
-  // TODO: more validation.
-
-  {
+static int load_object(obj_handle* obj, const char* bin, const char* filename) {
+  Elf_Ehdr* ehdr = (Elf_Ehdr*)bin;
   Elf_Shdr* shdrs = (Elf_Shdr*)(bin + ehdr->e_shoff);
   //Elf_Shdr* shstrtab = &shdrs[ehdr->e_shstrndx];
   Elf_Sym* symtab = NULL;
@@ -220,39 +300,8 @@ void* objopen(const char* filename, int flags) {
         expected_code_size = align_up(expected_code_size, 16);
       }
     }
-
-    for (int i = 0; i < ehdr->e_shnum; i++) {
-      Elf_Shdr* shdr = &shdrs[i];
-      int has_addend = shdr->sh_type == SHT_RELA;
-      size_t relsize = has_addend ? sizeof(Elf_Rela) : sizeof(Elf_Rel);
-      int relnum = shdr->sh_size / relsize;
-
-      if ((shdr->sh_type != SHT_REL && shdr->sh_type != SHT_RELA) ||
-          !(shdrs[shdr->sh_info].sh_flags & SHF_ALLOC)) {
-        continue;
-      }
-
-      for (int j = 0; j < relnum; j++) {
-        Elf_Rela* rel = (Elf_Rela*)(bin + shdr->sh_offset + relsize * j);
-        switch (ELF_R_TYPE(rel->r_info)) {
-#ifdef R_PLT32
-          case R_PLT32: {
-#if defined(__x86_64__)
-            expected_code_size += 6 + 8;
-#endif
-            break;
-          }
-#endif
-
-#if defined(__x86_64__)
-          case R_X86_64_REX_GOTPCRELX: {
-            expected_code_size += 8;
-            break;
-          }
-#endif
-        }
-      }
-    }
+    expected_code_size += relocate(obj, bin, symtab, strtab, addrs,
+                                   1 /* code_size_only */);
 
     obj->code_size = align_up(expected_code_size, 4096);
     obj->code = (char*)mmap(NULL, obj->code_size,
@@ -261,9 +310,7 @@ void* objopen(const char* filename, int flags) {
                             -1, 0);
     if (obj->code == MAP_FAILED) {
       sprintf(obj_error, "mmap failed: %s", strerror(errno));
-      free(bin);
-      free(obj);
-      return NULL;
+      return 0;
     }
 
 #if 0
@@ -322,109 +369,55 @@ void* objopen(const char* filename, int flags) {
     }
   }
 
-  for (int i = 0; i < ehdr->e_shnum; i++) {
-    Elf_Shdr* shdr = &shdrs[i];
-    int has_addend = shdr->sh_type == SHT_RELA;
-    size_t relsize = has_addend ? sizeof(Elf_Rela) : sizeof(Elf_Rel);
-    int relnum = shdr->sh_size / relsize;
-    char* target_base = addrs[shdr->sh_info];
+  relocate(obj, bin, symtab, strtab, addrs, 0 /* code_size_only */);
+  return 1;
+}
 
-    if ((shdr->sh_type != SHT_REL && shdr->sh_type != SHT_RELA) ||
-        target_base == NULL) {
-      continue;
-    }
+void* objopen(const char* filename, int flags) {
+  char* bin = NULL;
+  obj_handle* obj = NULL;
+  Elf_Ehdr* ehdr = NULL;
 
-    for (int j = 0; j < relnum; j++) {
-      Elf_Rela* rel = (Elf_Rela*)(bin + shdr->sh_offset + relsize * j);
-      char* target = target_base + rel->r_offset;
-      Elf_Sym* sym = &symtab[ELF_R_SYM(rel->r_info)];
-      int addend = has_addend ? rel->r_addend : 0;
-      char* sym_addr = NULL;
-      switch (ELF_ST_TYPE(sym->st_info)) {
-        case STT_SECTION:
-          sym_addr = addrs[sym->st_shndx];
-          break;
-
-        case STT_FUNC:
-        case STT_OBJECT:
-          sym_addr = (char*)sym->st_value;
-          break;
-
-        case STT_NOTYPE:
-          if (sym->st_shndx == SHN_UNDEF) {
-            sym_addr = (char*)dlsym(RTLD_DEFAULT, strtab + sym->st_name);
-            if (sym_addr == NULL) {
-              sprintf(obj_error, "failed to resolve %s", strtab + sym->st_name);
-              free(bin);
-              objclose(obj);
-              return NULL;
-            }
-          } else {
-            sym_addr = addrs[sym->st_shndx];
-          }
-          break;
-
-        default:
-          sprintf(obj_error, "unsupported relocation sym %d",
-                  ELF_ST_TYPE(sym->st_info));
-          free(bin);
-          objclose(obj);
-          return NULL;
-      }
-      //fprintf(stderr, "%d %s target=%p sym_addr=%p addend=%d\n",
-      //        j, strtab + sym->st_name, target, sym_addr, addend);
-
-      switch (ELF_R_TYPE(rel->r_info)) {
-#ifdef R_64
-        case R_64:
-          *(uint64_t*)target = (uint64_t)sym_addr + addend;
-          break;
+#if !OBJFCN_SPLIT_ALLOC
+  if (!code) {
+    init();
+  }
+  if (code == MAP_FAILED) {
+    return NULL;
+  }
 #endif
 
-#ifdef R_PC32
-        case R_PC32:
-          *(uint32_t*)target = (sym_addr - target) + addend;
-          break;
-#endif
-
-#ifdef R_PLT32
-        case R_PLT32: {
-          void* dest = sym_addr;
-#if defined(__x86_64__)
-          sym_addr = alloc_code(obj, 6 + 8);
-          sym_addr[0] = 0xff;
-          sym_addr[1] = 0x25;
-          *(uint32_t*)(sym_addr + 2) = 0;
-          *(uint64_t*)(sym_addr + 6) = (uint64_t)dest;
-#endif
-          *(uint32_t*)target = (sym_addr - target) + addend;
-          break;
-        }
-#endif
-
-#if defined(__x86_64__)
-        case R_X86_64_REX_GOTPCRELX: {
-          void* dest = sym_addr;
-          sym_addr = alloc_code(obj, 8);
-          *(uint64_t*)(sym_addr) = (uint64_t)dest;
-          *(uint32_t*)target = (sym_addr - target) + addend;
-          break;
-        }
-#endif
-
-        default:
-          sprintf(obj_error, "Unknown reloc: %ld", ELF_R_TYPE(rel->r_info));
-          free(bin);
-          objclose(obj);
-          return NULL;
-      }
-    }
+  bin = read_file(filename);
+  if (bin == NULL) {
+    return NULL;
   }
 
+  obj = (obj_handle*)malloc(sizeof(obj_handle));
+  if (obj == NULL) {
+    sprintf(obj_error, "malloc failed");
+    free(bin);
+    return NULL;
+  }
+  memset(obj, 0, sizeof(*obj));
+
+  ehdr = (Elf_Ehdr*)bin;
+  if (memcmp(ehdr->e_ident, ELFMAG, 4)) {
+    sprintf(obj_error, "%s is not ELF", filename);
+    free(bin);
+    free(obj);
+    return NULL;
   }
 
-  free(bin);
-  return obj;
+  // TODO: more validation.
+
+  if (load_object(obj, bin, filename)) {
+    free(bin);
+    return obj;
+  } else {
+    free(bin);
+    objclose(obj);
+    return NULL;
+  }
 }
 
 int objclose(void* handle) {
