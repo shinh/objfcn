47909e496177a8eb8b57f63c20ed59fd0b899733
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2020-02-12 21:29:21 +0900

    More complex test for TLS

--- complex.cc
+++ complex.cc
@@ -1,10 +1,15 @@
 namespace {
-thread_local int g_tls_var = 140;
+thread_local int g_tls_var = 20;
+thread_local int g_tls_var2 = 120;
+thread_local int g_tls_bss;
+thread_local int g_tls_bss2;
 }
 
 extern "C" {
   int func(int x) {
     g_tls_var++;
-    return x + g_tls_var;
+    g_tls_bss -= 3;
+    g_tls_bss2 += 3;
+    return x + g_tls_var + g_tls_var2 + g_tls_bss + g_tls_bss2;
   }
 }
